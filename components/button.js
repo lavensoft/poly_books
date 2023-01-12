239fb768a25c7dd34bb3dbe1587283a9b96fd123
lavensoft <nhatsdevil@gmail.com>
2023-01-12 23:25:14 +0700

    first commit

---
+++ components/button.js
@@ -0,0 +1,3 @@
+export const Button = () => {
+    return <h1>H1HELLO</h1>
+}
---
+++ pages/home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+export const HomeView = () => {
+    return (
+        <div className={styles.view}>
+            <h1 className={styles.title}>HELLO WORLD!!!</h1>
+        </div>
+    )
+}
