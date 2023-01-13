9ca4a13747ab621fafccc63fcd35db5be3e35099
khaiminh0401 <khaiminh0401@gmail.com>
2023-01-13 17:56:34 +0700

    Done task. Complete ButtonSection, Carousel, ButtonDetailSection

--- components/Carousel/index.js
+++ components/Carousel/index.js
@@ -1,12 +1,5 @@
-import Image from "next/image";
-// import Carousel from "react-bootstrap/Carousel";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import book1 from '../../public/image/book1.jpg';
-import book2 from '../../public/image/book2.jpg';
-import book3 from '../../public/image/book3.jpg';
-import style from './carousel.module.scss';
-import { config } from "@config";
 function CustomCarousel({responsive,children}) {
     return (
         <Carousel 
--- components/Sections/BookDetailSection/index.js
+++ components/Sections/BookDetailSection/index.js
@@ -1,13 +1,22 @@
-import { Carousel } from "@components";
+import { Button, Carousel } from "@components";
 import Title from "@components/Title";
 import { config } from "@config";
 import { Col, Row } from "react-bootstrap";
-
+import style from './index.module.scss'
 function BookDetailSection() {
+    
+    const mock = ["Top selling", "Deals", "New releases in fiction", "New releases in nonfiction", "Top free"];
+    const btn = mock.map((s, index) => {
+        return s=="Top selling"? <Button key={index} className={`mx-3 my-2 ${style.btn} ${style.active} `} variant={"outline-dark"}>{s}</Button>:<Button key={index} className={`mx-3 my-2 ${style.btn}`} variant={"outline-dark"}>{s}</Button>
+    })
     return (
         <Row>
             <Title text={"Top chart"} />
+            <div>
+                {btn}
+            </div>
             <Carousel responsive={config.responsive.BookDetailSection}>
+
                 <Row>
                     <Col xs={12} sm={6} md={3}>
                         <div className="card mx-3 my-3">
